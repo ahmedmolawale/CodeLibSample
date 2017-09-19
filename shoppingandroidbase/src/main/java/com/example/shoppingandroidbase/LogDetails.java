76ecb5f31f9c654f3447b643a3cb3d1a6ad6e605
Olawale <ahmedmolawale@gmail.com>
2017-09-19 05:08:12 +0100

    Create a sample code library

---
+++ shoppingandroidbase/src/main/java/com/example/shoppingandroidbase/LogDetails.java
@@ -0,0 +1,16 @@
+package com.example.shoppingandroidbase;
+
+import android.util.Log;
+
+/**
+ * Created by stackcode on 9/19/17.
+ */
+
+public class LogDetails {
+
+    private String TAG = "SUPER EASY";
+
+    public void d(){
+        Log.d(TAG,"Hey!!! Its my first android Lib Ever");
+    }
+}
